09dc07d918ea1594f2ae047f61da71df48153879
Steven Isbell <steven.isbell18@gmail.com>
2017-11-16 21:35:37 -0600

    updating

--- angular/app.js
+++ angular/app.js
@@ -21,5 +21,23 @@
         domain: "devmountain.auth0.com",
         clientID: "3SUPvC77-j6j5iIz8qRGVya_lWH43Wis"
       });
+      $urlRouterProvider.otherwise("/home");
+
+      $stateProvider
+        .state("home", {
+          url: "/home",
+          templateUrl: "components/home/home.tpl.html"
+        })
+        .state("profile", {
+          url: "/profile",
+          templateUrl: "components/profile/profile.tpl.html",
+          controller: "profileController as user"
+        });
+
+      jwtInterceptorProvider.tokenGetter = function(store) {
+        return store.get("token");
+      };
+
+      $httpProvider.interceptors.push("jwtInterceptor");
     });
 })();
